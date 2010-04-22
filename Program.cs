214027f76c2f62f285e1030a8fe25183899121b9
Caesar Kabalan <Dandelock@gmail.com>
2010-04-22 23:23:57 +0000

    Client: Began redesigning Client interface. ToolStrip and StatusStrip are both "done". No functionality yet.
    git-svn-id: file:///content/svn/csharp/boobox/BooBoxClient@17 df8029df-fac5-43eb-a43f-3a0073f54a4e

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace BooBoxClient {
 		static void Main() {
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Form1());
+			Application.Run(new MainFrm());
 		}
 	}
 }
